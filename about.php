4b9f00d3fabdfbe95a39de28cfd791e9071c5759
Sac2132 <64259340+Sac2132@users.noreply.github.com>
2020-04-24 16:51:37 +0530

    Pulbising my first Website
    Yeah! I am Done so exicted

---
+++ about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Rokkitt&display=swap" rel="stylesheet"></head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">$aurabh Choubey</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Contacts</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1 class="display-4">Saurabh Choubey</h1>
+  <p class="lead">Awesome Guyz We Did it and Finally The website is created and I love you guyzz..</p>
+  <hr class="my-4">
+  <p>My name is Saurabh Arvind Choubey. I'm 21years old. I born in Titwala, Thane city. Currently, I am living in titwala, India. In my family, I live with my papa, mummy and one cute sister, her name is Binita Thapa. I am thankful to God that he gives me such a wonderful family.
+I am working as a web developer and I am a Youtuber too. I want to share with you my experiences during Colleges, as Web developers and the problems I faced, the solutions I found and the places I went to.
+I hope my blog will guide you, and give you the knowledge that you are looking to...</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+<section class="my-5">
+	<div class="py-5">
+		<h3 class="text-center">About us</h3>
+	</div>
+	<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-12">
+			<img src="images/c5.jpg" class="img-fluid aboutimg">
+		</div>
+		<div class="col-lg-6 col-md-6 col-12">
+			<h2 class="display-4">I am Saurabh Choubey.</h2>
+			<p class="py-3">
+				This is My First Web Page Designing And Stay Home And Stay Safe.when you don't create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.
+			</p>
+			<a href="about.php" class="btn btn-success"> Wanna Know me </a>
+		</div>
+	</div>
+</div>
+</section>
+<footer>
+	<p class="p-3 bg-dark text-white text-center">@SaurabhWorlds</p>
+</footer>
+</body>
+</html>
+
